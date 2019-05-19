5fb44bf8c9fb3b71884e39cfd2bf406d636af118
WilhelmGY <wilhemd.gy@gmail.com>
2019-05-19 16:20:27 +0200

    Kan fylla rutor (Färg ej fungerande)

---
+++ FyraIRad.js
@@ -0,0 +1,20 @@
+function placeranod(x) {
+  for (var y = 5; y >= 0; y--) {
+    var a = document.getElementById("Rutnat").rows[y].cells[x];
+    //setAttribute("class", "gulruta");
+    if (a.innerHTML == "") {//style.backgroundColor == none
+      a.innerHTML = "Gul";//Ta bort när funktionen fungerar
+      var s = document.getElementById('Spelare').style.color;
+      if (s == yellow) {
+        a.innerHTML = "Gul";
+        //a.style.backgroundColor = yellow;
+        s = blue;
+        break;
+      }else if (s == blue) {
+        a.innerHTML = "Blå";
+        s = yellow;
+        break;
+      }
+    }
+  }
+}
