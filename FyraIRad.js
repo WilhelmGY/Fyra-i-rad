bafb13095e5dc28ba9eee1284c43b17226fdc39c
WilhelmGY <wilhemd.gy@gmail.com>
2019-05-19 17:23:21 +0200

    sista ver

--- FyraIRad.js
+++ FyraIRad.js
@@ -4,14 +4,16 @@ function placeranod(x) {
     //setAttribute("class", "gulruta");
     if (a.innerHTML == "") {//style.backgroundColor == none
       a.innerHTML = "Gul";//Ta bort när funktionen fungerar
-      var s = document.getElementById('Spelare').style.color;
+      var s = document.getElementById("Spelare").style.color;
       if (s == yellow) {
         a.innerHTML = "Gul";
         //a.style.backgroundColor = yellow;
+        document.getElementById("Spelare").text = "Gul spelare";
         s = blue;
         break;
       }else if (s == blue) {
         a.innerHTML = "Blå";
+        document.getElementById("Spelare").text = "Blå spelare";
         s = yellow;
         break;
       }
